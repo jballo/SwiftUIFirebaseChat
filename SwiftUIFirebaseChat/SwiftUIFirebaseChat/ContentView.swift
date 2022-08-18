9202d7e224fea072c7aedf3074e0b684a5181ebe
jballo <jballona5127@gmail.com>
2022-08-18 12:47:02 -0700

    Project Creation
    - Interface: SwiftUI
    - Language: Swift

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Jonathan Ballona Sanchez on 8/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Jonathan Ballona Sanchez on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
